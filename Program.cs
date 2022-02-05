d3ba086d4c6bc478ad7cdc7d813a5d0779298199
Pondworth <amy@pondworth.de>
2022-02-05 13:38:10 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace HalloWelt
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hallo Welt!");
+            Console.WriteLine("Ich nutzte dies, um meinen Lernfortschritt und generell meinen Coding-Stand festzuhalten.");
+            Console.WriteLine("Angefangen mit einem kleinen Hallo Welt, bis hin zu kleinen Programmen die man ausführen kann.");
+            Console.WriteLine("Das ist vielleicht nichts nützliches für euch, aber für mich, allein aus dokumentarischer Sicht.");
+            Console.WriteLine("Danke für Eure Aufmerksamkeit, bis zum nächsten Mal, Eure Amy");
+            Console.ReadKey();
+        }
+    }
+}
